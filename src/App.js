0351da8e77ecd916fab1360286d4ef2324ec0ca6
nmaz-code <nmaz@accepted.gr>
2025-11-03 18:44:49 +0200

    section 4

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 
-import './App.css';
-
+import './App.css'
+import { useState } from 'react'
 
 
 function App() {
+  const [name, setName] = useState('mario')
+
+  const handleClick = () => {
+      setName('luigi')
+      console.log(name)
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src='/logo.svg' className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React & Firebase
-        </a>
-      </header>
+      <h1>My name is {name}</h1>
+      <button onClick={handleClick}>Change name</button>
     </div>
   );
 }
