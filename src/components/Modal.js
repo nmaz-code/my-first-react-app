29757b12b74b61de41ffe88f8263af99ccf44e99
nmaz-code <nmaz@accepted.gr>
2025-11-07 12:20:39 +0200

    lesson 44

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Modal.css';
 import ReactDOM from 'react-dom';
 
-export default function Modal({children, handleClose, isSalesModal}) {
+export default function Modal({children,  isSalesModal}) {
   // debug: verify children are passed
   
   return ReactDOM.createPortal((
@@ -13,9 +13,7 @@ export default function Modal({children, handleClose, isSalesModal}) {
       }}>
         <div className="modal">
            {children}
-        <button 
-          onClick={handleClose} 
-          className={isSalesModal ? "sales-btn" : ""}>close</button>
+        
         </div> 
     </div>
   ), document.body)
